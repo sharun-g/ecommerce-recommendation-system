bb3750b0512d0cff67731c19db324b1616361ebd
Sharun Gunti <guntisharun0476@gmail.com>
2026-04-05 22:50:03 +0530

    loads data

---
+++ src/data_loader.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def load_data():
+    orders = pd.read_csv(
+        "data/orders.csv",
+        usecols=["order_id", "user_id"],
+        dtype={"order_id": "int32", "user_id": "int32"}
+    )
+
+    order_products = pd.read_csv(
+        "data/order_products__prior.csv",
+        usecols=["order_id", "product_id"],
+        dtype={"order_id": "int32", "product_id": "int32"}
+    )
+
+    return orders, order_products
