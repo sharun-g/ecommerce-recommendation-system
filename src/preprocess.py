fa3ce80b70a7f0e5942bdbb8daa0ada9a68c0a92
Sharun Gunti <guntisharun0476@gmail.com>
2026-04-29 22:40:21 +0530

    preprocess the data

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,14 +2,11 @@ import pandas as pd
 
 def preprocess(orders, order_products, user_limit=20000, product_limit=1000):
 
-    # Select active users
     top_users = orders['user_id'].value_counts().head(user_limit).index
     orders = orders[orders['user_id'].isin(top_users)]
 
-    # Merge
     data = pd.merge(order_products, orders, on="order_id")
 
-    # Select popular products
     top_products = data['product_id'].value_counts().head(product_limit).index
     data = data[data['product_id'].isin(top_products)]
 
