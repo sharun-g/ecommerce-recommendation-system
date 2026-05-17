2f1fb1f25eea5b5227f2c8b8a0bd2f84077e638c
Sharun Gunti <guntisharun0476@gmail.com>
2026-05-17 12:26:11 +0530

    Update comments and save product ID mapping

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+# Import required modules and functions
 from src.data_loader import load_data
 from src.preprocess import preprocess
 from src.fp_growth_model import run_fp_growth
@@ -6,24 +7,36 @@ from src.cf_model import build_cf
 import pickle
 
 print("Loading data...")
+
+# Load datasets
 orders, order_products, products = load_data()
 
 print("Preprocessing...")
+
+# Clean and prepare transaction data
 data = preprocess(orders, order_products)
 
+# Save processed dataset
 data.to_csv("data/processed_data.csv", index=False)
 
 print("Running FP-Growth...")
+
+# Generate association rules using FP-Growth
 rules = run_fp_growth(data)
+
+# Save FP-Growth rules
 rules.to_pickle("data/fp_rules.pkl")
 
 print("Building CF model...")
+
+# Build Collaborative Filtering similarity matrix
 similarity, product_ids = build_cf(data)
 
+# Save similarity matrix
 with open("data/similarity.pkl", "wb") as f:
     pickle.dump(similarity, f)
 
-# Save product_ids mapping too
+# Save product ID mapping
 with open("data/product_ids.pkl", "wb") as f:
     pickle.dump(product_ids, f)
 
